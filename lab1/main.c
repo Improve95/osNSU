baaca2ecbb4764b18d25659cfd85fd78a9443615
e.davydov <e.davydov@g.nsu.ru>
2024-02-02 14:55:14 +0700

    l1

---
+++ lab1/main.c
@@ -0,0 +1,5 @@
+#include "slhw.h"
+
+int main() {
+    hello_from_static_lib();
+}
---
+++ lab1/static-lib-hw.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void hello_from_static_lib() {
+    printf("Hello from static library world\n");
+}
+
