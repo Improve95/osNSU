d68f3831fe9f9e0e55c56fc287e25a077edfb3b8
e.davydov <e.davydov@g.nsu.ru>
2024-02-02 15:50:03 +0700

    стаический хелло

---
+++ lab1/staticLib/hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "staticHello.h"
+
+int main() {
+    staticHello();
+    return 0;
+}
---
+++ lab1/staticLib/staticHello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void staticHello() {
+    printf("Static hello!");
+}
