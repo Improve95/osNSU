4257dfb5faf3dbce0913529ea80b235a8cbf1b83
e.davidov@ <e.davidov@g.nsu.ru>
2024-04-10 22:40:19 +0700

    l4

---
+++ lab4/task3/t3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <string.h>
+#include <signal.h>
+
+void handler(int signum) {
+    
+}
+
+
+void my_free() {
+
+}
+
+void * my_malloc() {
+
+}
+
+int main() {
+
+}
