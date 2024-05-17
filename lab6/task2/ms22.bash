46dfb56f28b0f59dc50867c7bdbd746d03b2d366
e.davydov-server <e.davydov@g.nsu.ru>
2024-05-17 10:06:14 +0700

    l6

--- lab6/task2/ms22.bash
+++
@@ -1,5 +0,0 @@
-rm t22.out
-clear
-
-gcc t22.c -Wall -Werror -Wextra -o t22.out
-./t22.out
--- lab6/task2/t21.c
+++ lab6/task2/t21.c
@@ -11,8 +11,38 @@
 #include <sys/stat.h>
 #include <errno.h>
 
+#define SIZE 128
+
 int main() {
-    
+    int fd_list[2];
+
+    int err = pipe(fd_list);
+    if (err == - 1) {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
+
+    int fork_pid = fork();
+    if (fork_pid == -1) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+
+    if (fork_pid == 0) {
+        char val;
+        while (1) {
+            for (int i = 0; i < SIZE * 2; ++i) {
+                read(fd_list[0], &val, sizeof(val));
+                printf("val: %c\n", val);
+            }
+        }
+    } else {
+        while (1) {
+            for (int i = 0; i < SIZE * 2; ++i) {
+                write(fd_list[1], &i, sizeof(char));
+            }
+        }
+    }
     
     return 0;
 }
