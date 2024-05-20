a2bd73be701eda7e55289bc2c8608f70d031a340
e.davydov-server <e.davydov@g.nsu.ru>
2024-05-20 14:15:46 +0700

    l5

---
+++ lab6/task3/client.bash
@@ -0,0 +1,5 @@
+rm client.out
+clear
+
+gcc client.c -Wall -Werror -Wextra -o client.out
+./client.out
---
+++ lab6/task3/server.bash
@@ -0,0 +1,5 @@
+rm server.out
+clear
+
+gcc server.c -Wall -Werror -Wextra -o server.out
+./server.out
