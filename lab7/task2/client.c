2e97e1a9cd18d45ff2cddd23b63039cbb5c70068
e.davydov-server <e.davydov@g.nsu.ru>
2024-05-24 13:43:16 +0700

    l7

--- lab7/task2/client.c
+++ lab7/task2/client.c
@@ -19,31 +19,44 @@
 #define BUFFER_SIZE 1024
 
 int main() {
-    struct sockaddr_in srv_sockaddr;
-    socklen_t clt_sock_len = sizeof(srv_sockaddr);
+    struct sockaddr_in clt_sockaddr;
+    socklen_t clt_sock_len = sizeof(clt_sockaddr);
     int client_fd;
     int status;
 
-    client_fd = socket(AF_INET, SOCK_DGRAM, 0);
+    client_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (client_fd == -1) {
         perror("open socket");
         exit(EXIT_FAILURE);
     }
 
-    status = inet_pton(AF_INET, "127.0.0.1", &srv_sockaddr.sin_addr);
+    status = inet_pton(AF_INET, "127.0.0.1", &clt_sockaddr.sin_addr);
     if (status == -1) {
         perror("inet_pton");
         exit(EXIT_FAILURE);
     }
-    srv_sockaddr.sin_port= htons(PORT);
-    srv_sockaddr.sin_family = AF_INET;  
+    clt_sockaddr.sin_port= htons(PORT);
+    clt_sockaddr.sin_family = AF_INET;
+
+    status = connect(client_fd, (struct sockaddr_in*)&clt_sockaddr, clt_sock_len);
+    if (status == -1) {
+        perror("connect");
+        exit(EXIT_FAILURE);
+    }   
 
     char send_buffer[BUFFER_SIZE], receive_buffer[BUFFER_SIZE];
+    size_t receive_buffer_size = BUFFER_SIZE;
     for (int i = 0; i < 5; i++) {
         snprintf(send_buffer, BUFFER_SIZE, "hello_%d", i);
-        sendto(client_fd, send_buffer, BUFFER_SIZE, 0, (struct sockaddr*)&srv_sockaddr, clt_sock_len);
+        if (write(client_fd, send_buffer, BUFFER_SIZE) == -1) {
+            perror("write");
+            exit(EXIT_FAILURE);
+        }
 
-        recvfrom(client_fd, receive_buffer, BUFFER_SIZE, 0, (struct sockaddr*)&srv_sockaddr, &clt_sock_len);
+        if (read(client_fd, receive_buffer, receive_buffer_size) == -1) {
+            perror("read");
+            exit(EXIT_FAILURE);
+        };
         printf("%s\n", receive_buffer);
 
         sleep(2);
--- lab7/task2/server.c
+++ lab7/task2/server.c
@@ -21,11 +21,13 @@
 
 int main() {
     struct sockaddr_in srv_sockaddr, clt_sockaddr;
-    socklen_t srv_sock_len = sizeof(srv_sockaddr);
+    socklen_t len = sizeof(srv_sockaddr);
+    int server_fd;
     int client_fd;
+    int fork_pid;
 
-    client_fd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (client_fd == -1) {
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == -1) {
         perror("open socket");
         exit(EXIT_FAILURE);
     }
@@ -34,16 +36,31 @@ int main() {
     srv_sockaddr.sin_addr.s_addr = INADDR_ANY;
     srv_sockaddr.sin_port = htons(PORT);
 
-    if (bind(client_fd, (struct sockaddr*)&srv_sockaddr, sizeof(srv_sockaddr)) == -1) {
-        perror("bind");
+    bind(server_fd, (struct sockaddr*)&srv_sockaddr, sizeof(srv_sockaddr));
+
+    if (listen(server_fd, 5) == -1) {
+        perror("listen");
         exit(EXIT_FAILURE);
+    }
+
+    while (true) {
+        client_fd = accept(server_fd, (struct sockaddr*)&clt_sockaddr, &len);
+        if (client_fd == -1) {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+
+        fork_pid = fork();
+        if (fork_pid == 0) {
+            break;
+        }
     }
 
     char receive_buffer[BUFFER_SIZE];
     char send_buffer[BUFFER_SIZE + 30];
     int read_bytes;
     while (true) {
-        read_bytes = recvfrom(client_fd, receive_buffer, BUFFER_SIZE, 0, (struct sockaddr*)&clt_sockaddr, &srv_sock_len);
+        read_bytes = read(client_fd, receive_buffer, BUFFER_SIZE);
         if (read_bytes == -1) {
             perror("read");
             exit(EXIT_FAILURE);
@@ -51,11 +68,12 @@ int main() {
 
         printf("%s\n", receive_buffer);
         snprintf(send_buffer, BUFFER_SIZE + 30, "return from server: %s", receive_buffer);
-        sendto(client_fd, send_buffer, BUFFER_SIZE, 0, (struct sockaddr*)&clt_sockaddr, srv_sock_len);
+        send(client_fd, send_buffer, BUFFER_SIZE, 0);
     }
+
     
     close(client_fd);
-    close(client_fd);
+    close(server_fd);
 
     return EXIT_SUCCESS;
 }
