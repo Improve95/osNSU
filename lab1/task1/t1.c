53bd68d1d6af57191a39a9d6c92b1968921164da
e.davydov-server <e.davydov@g.nsu.ru>
2024-09-06 23:28:50 +0700

    l1 сделал часть, осталось разобраться в происходящем

--- lab1/task1/t1.c
+++ lab1/task1/t1.c
@@ -6,24 +6,44 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+int global = 5;
+static int global_static = 10; 
+
 void *mythread(void *arg) {
-	printf("mythread [%d %d %d]: Hello from mythread!\n", getpid(), getppid(), gettid());
+	int local = 15;
+	static int local_static = 20;
+	const int local_const = 25;
+
+	printf("mythread [%d %d %d %ld]: Hello from mythread!\nVariables: global %p, global_static %p local %p local_static %p local_const %p\n", 
+		getpid(), getppid(), gettid(), pthread_self(), &global, &global_static, &local, &local_static, &local_const);
 	return NULL;
 }
 
 int main() {
-	pthread_t tid;
+	pthread_t tid[5];
 	int err;
 
-	printf("main [%d %d %d]: Hello from main!\n", getpid(), getppid(), gettid());
+	int local = 15;
+	static int local_static = 20;
+	const int local_const = 25;
 
-	err = pthread_create(&tid, NULL, mythread, NULL);
-	if (err) {
-	    printf("main: pthread_create() failed: %s\n", strerror(err));
-		return -1;
+	printf("main [%d %d %d]: Hello from main!\nVariables: global %p, global_static %p local %p local_static %p local_const %p\n\n", 
+		getpid(), getppid(), gettid(), &global, &global_static, &local, &local_static, &local_const);
+
+	for (int i = 0; i < 5; i++) {
+		err = pthread_create(&tid[i], NULL, mythread, NULL);
+		if (err) {
+			printf("main: pthread_create() failed: %s\n", strerror(err));
+			return -1;
+		}
 	}
 
-    sleep(2);
+	sleep(1);
+
+	printf("\n");
+	
+	printf("ref threads is equal: %d\n", tid[0] == tid[1]);
+	printf("thread is equal: %d\n", pthread_equal(tid[0], tid[1]));
 
 	return 0;
 }
