f847a15cc736a2ec42522a933c943d46781df121
e.davydov-server <e.davydov@g.nsu.ru>
2024-09-12 23:21:35 +0700

    commit

--- lab1/task1/ms.bash
+++ lab1/task1/ms.bash
@@ -1,7 +1,7 @@
 clear
 
-# gcc t1.c -o t1 -lpthread -Werror -Wextra
-# ./t1
-
-gcc t11.c -o t1 -lpthread -Werror -Wextra
+gcc t1.c -o t1 -lpthread -Werror -Wextra
 ./t1
+
+# gcc t11.c -o t1 -lpthread -Werror -Wextra
+# ./t1
