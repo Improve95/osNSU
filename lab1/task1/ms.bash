778c6433bc09a4d0dc44f41828fc3e25d360e22a
e.davydov-server <e.davydov@g.nsu.ru>
2024-09-17 17:55:01 +0700

    t14

--- lab1/task1/ms.bash
+++ lab1/task1/ms.bash
@@ -4,8 +4,8 @@ clear
 # gcc t1.c -o t.out -lpthread -Werror -Wextra
 # gcc t11.c -o t.out -lpthread -Werror -Wextra
 # gcc t12.c -o t.out -lpthread -Werror -Wextra
-gcc t12d.c -o t.out -lpthread -Werror -Wextra
+# gcc t12d.c -o t.out -lpthread -Werror -Wextra
 # gcc t12e.c -o t.out -lpthread -Werror -Wextra
-# gcc t13.c -o t.out -lpthread -Werror -Wextra
+gcc t13.c -o t.out -lpthread -Werror -Wextra
 
 ./t.out
--- lab1/task1/t13.c
+++ lab1/task1/t13.c
@@ -17,24 +17,28 @@ typedef struct {
 } test_struct;
 
 void *mythread(void *args) {
-    test_struct *test = (test_struct *) args;
+    test_struct *give_struct = (test_struct *) args;
     printf("mythread [%d %d %d]: Hello from mythread!\n", getpid(), getppid(), gettid());
-    print("mythread [%d %s]", test->number, test->char_array);
+    printf("mythread [%d %s]\n", give_struct->number, give_struct->char_array);
     sleep(3);
 }
 
 int main() {
     int err;
-    pthread_attr_t attr;
     pthread_t tid;
 
-    test_struct *test = malloc(sizeof(test_struct));
-    test->number = 10;
-    test->char_array = malloc(30 * sizeof(char));
-    snprintf(test->char_array, 30, "hello world from struct");
+    test_struct stack_struct;
+    stack_struct.number = 10;
+    stack_struct.char_array = malloc(30 * sizeof(char));
+    snprintf(stack_struct.char_array, 30, "hello world from stack struct");
+
+    test_struct *heap_struct = malloc(sizeof(test_struct));
+    heap_struct->number = 20;
+    heap_struct->char_array = malloc(30 * sizeof(char));
+    snprintf(heap_struct->char_array, 30, "hello world from heap struct");
 
     printf("main [%d %d %d]: Hello from main!\n", getpid(), getppid(), gettid());
-    err = pthread_create(&tid, &attr, mythread, (void *)test);
+    err = pthread_create(&tid, NULL, mythread, (void *)heap_struct);
     if (err) {
         perror("pthread_create");
         exit(EXIT_FAILURE);
