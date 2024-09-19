926fab631653e3f3ffb9f460ec977f3dca653968
e.davydov-server <e.davydov@g.nsu.ru>
2024-09-19 23:12:43 +0700

    lab14

--- lab1/task1/t14c.c
+++ lab1/task1/t14c.c
@@ -11,14 +11,23 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
+void cleanup(void *arg) {
+    free(arg);
+    printf("hello from cleanup\n");
+}
+
 void *mythread(void *args) {
     char *str = malloc(30);
     snprintf(str, 30, "hello world from thread\n");
     
+    pthread_cleanup_push(cleanup, str);
+
     while (1) {
         printf("%s", str);
         sleep(1);
     }
+
+    pthread_cleanup_pop(0);
 }
 
 int main() {
@@ -32,8 +41,8 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    // sleep(5);
-    // pthread_cancel(tid);
+    sleep(2);
+    pthread_cancel(tid);
 
     pthread_join(tid, NULL);
     return 0;
