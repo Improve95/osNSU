e55fa4bdac403ddb9e947412e87ad58d9186edd9
Egor Davydov <88789407+Improve95@users.noreply.github.com>
2024-09-25 17:36:32 +0700

    Update main.c

--- lab1/task2/src/main.c
+++ lab1/task2/src/main.c
@@ -6,10 +6,10 @@ void *mythread_function(void *arg) {
 }
 
 int main() {
-    __mythread tid;
+    __mythread *tid;
     int err;
     
-    err = mythread_create(&tid, mythread_function, NULL);
+    err = mythread_create(tid, mythread_function, NULL);
     if (err != 0) {
         perror("mythread_create");
         return err;
