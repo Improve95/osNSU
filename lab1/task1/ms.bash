fef7c47e2e8eb5b8ac7a668d2ebba942bd1b7c31
e.davydov <e.davydov@g.nsu.ru>
2024-10-03 22:48:56 +0700

    t5

--- lab1/task1/ms.bash
+++ lab1/task1/ms.bash
@@ -7,9 +7,11 @@ clear
 # gcc t12d.c -o t.out -lpthread -Werror -Wextra
 # gcc t12e.c -o t.out -lpthread -Werror -Wextra
 # gcc t13.c -o t.out -lpthread -Werror -Wextra
-gcc t13a.c -o t.out -lpthread -Werror -Wextra
+# gcc t13a.c -o t.out -lpthread -Werror -Wextra
 # gcc t14a.c -o t.out -lpthread -Werror -Wextra
 # gcc t14b.c -o t.out -lpthread -Werror -Wextra
 # gcc t14c.c -o t.out -lpthread -Werror -Wextra
+
+gcc t15.c -o t.out -lpthread -Werror -Wextra
 
 ./t.out
