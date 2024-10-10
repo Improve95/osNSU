37cc7e8dcb626ec1431f4fbde21151178a2f838a
e.davydov <e.davydov@g.nsu.ru>
2024-10-10 21:15:22 +0700

    l1t7 initial

---
+++ lab1/task3/t7.c
@@ -0,0 +1,18 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <errno.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
+
+
+
+int main() {
+
+    return 0;
+}
