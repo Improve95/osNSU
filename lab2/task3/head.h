43d7d7ed8ed4a043509263010dd28560c8b65cc9
e.davydov <e.davydov@g.nsu.ru>
2024-11-07 12:58:34 +0700

    commit

--- lab2/task3/head.h
+++ lab2/task3/head.h
@@ -4,10 +4,12 @@
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 #include <string.h>
 #include <errno.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <semaphore.h>
+#include <pthread.h>
+#include <sched.h>
 
 #endif
--- lab2/task3/t3.c
+++ lab2/task3/t3.c
@@ -4,17 +4,36 @@
 
 volatile int operation_count;
 
-void *routine1() {
+void *start_routine1(void *arg) {
+    Node* linked_list = (Node*) arg;
 
 }
 
-void *routine2() {
+void *start_routine2(void *arg) {
+    Node* linked_list = (Node*) arg;
 
 }
 
-void *routine3() {
+void *start_routine3(void *arg) {
+    Node* linked_list = (Node*) arg;
 
 }
+
+void *start_routine4(void *arg) {
+    Node* linked_list = (Node*) arg;
+
+}
+
+void *start_routine5(void *arg) {
+    Node* linked_list = (Node*) arg;
+
+}
+
+void *start_routine6(void *arg) {
+    Node* linked_list = (Node*) arg;
+
+}
+
 
 Node* generate_new_node() {
     int string_size = rand() % MAX_VALUE_SIZE;
@@ -24,9 +43,15 @@ Node* generate_new_node() {
 }
 
 int main() {
+    int err = 0;
+
     pthread_t tid1;
     pthread_t tid2;
     pthread_t tid3;
+
+    pthread_t tid4;
+    pthread_t tid5;
+    pthread_t tid6;
 
     Node* linked_list = NULL;
     const int list_size = 10;
@@ -43,6 +68,32 @@ int main() {
         printf("%ld\n", strlen(tmp->value));
         tmp = tmp->next;
     }
+
+    err = pthread_create(&tid1, NULL, start_routine1, linked_list);
+    if (err) perror("pthread_create tid1");
+
+    err = pthread_create(&tid2, NULL, start_routine2, linked_list);
+    if (err) perror("pthread_create tid2");
+
+    err = pthread_create(&tid3, NULL, start_routine3, linked_list);
+    if (err) perror("pthread_create tid3");
+
+    err = pthread_create(&tid4, NULL, start_routine4, linked_list);
+    if (err) perror("pthread_create tid4");
+
+    err = pthread_create(&tid5, NULL, start_routine5, linked_list);
+    if (err) perror("pthread_create tid5");
+    
+    err = pthread_create(&tid6, NULL, start_routine6, linked_list);
+    if (err) perror("pthread_create tid6");
+
+    pthread_join(tid1, NULL);
+    pthread_join(tid2, NULL);
+    pthread_join(tid3, NULL);
+
+    pthread_join(tid4, NULL);
+    pthread_join(tid5, NULL);
+    pthread_join(tid6, NULL);
 
     return 0;
 }
